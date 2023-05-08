6eff82dd0f3910624c43245b2141f640b248d031
Marius Ose Wagner <mw@emqu.cc>
2023-05-08 16:54:37 +0200

    Applet now works if there's only Flatpak updates

--- swarofi-applet.sh
+++ swarofi-applet.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 check_updates() {
-    upgrade_check_output=$(rpm-ostree upgrade --check 2>&1)
+    upgrade_check_output=$(rpm-ostree upgrade --check 2>&1 || true)
     available_updates=$(echo "$upgrade_check_output" | grep 'AvailableUpdate:' || true)
     flatpak_updates_output=$(flatpak remote-ls --updates 2>&1)
     num_flatpak_updates=$(echo "$flatpak_updates_output" | grep -oP '^[^\s]+\s+[^\s]+' | wc -l)
@@ -15,12 +15,8 @@ check_updates() {
     if [ "$num_rpm_ostree_updates" -gt 0 ] || [ "$num_flatpak_updates" -gt 0 ]; then
         total_updates=$((num_rpm_ostree_updates + num_flatpak_updates))
 
-        version=$(echo "$upgrade_check_output" | grep -oP '(?<=Version: )[^\s]+' || echo "N/A")
-        commit=$(echo "$upgrade_check_output" | grep -oP '(?<=Commit: )[^\s]+' || echo "N/A")
-        gpg_signature=$(echo "$upgrade_check_output" | grep -oP '(?<=GPGSignature: )[^\n]+' || echo "N/A")
-
         update_icon=""
-        message="Version: $version | Commit: $commit | GPGSignature: $gpg_signature | Diff: $num_rpm_ostree_updates rpm-ostree, $num_flatpak_updates Flatpak | Total: $total_updates upgraded"
+        message="RPM-OSTree updates: $num_rpm_ostree_updates | Flatpak updates: $num_flatpak_updates"
         echo "$update_icon $total_updates"
         echo "$message"
         notify-send "System Update Available" "$message"
